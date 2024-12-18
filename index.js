43fac0de93f3ad15be8bb48d0f73027230ff186b
dnesgurung <174604785+dnesgurung@users.noreply.github.com>
2024-12-18 13:52:43 +0000

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function greetGuest(name) {
-
-    if ( name === ''){
-        return `Hello !`
-    } else {
-   let capitalise = String(name[0]).toUpperCase() + String(name).slice(1);
-    return `Hello ${capitalise}!`;
-    }
-    }
