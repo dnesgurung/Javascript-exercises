7769e9be9aff75f0eb32fc9fb6bd5946fd9708ad
dnesgurung <174604785+dnesgurung@users.noreply.github.com>
2024-12-24 10:34:11 +0000

    Create index.js

---
+++ Beginners-exercises/FInd-even-length-strings/index.js
@@ -0,0 +1,10 @@
+function findEvenLengthStrings(items){
+
+const result = [];
+  for (let i = 0; i < iems.length; i++ ){
+    if(typeof items[i] === 'string' && items[i].length % 2 === 0){
+      result.push(items[i];
+    }
+  }
+  return result;
+}
