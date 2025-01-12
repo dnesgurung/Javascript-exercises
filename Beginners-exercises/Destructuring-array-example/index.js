4349115dc831606f35da5b21caa09479dc6a11de
dnesgurung <174604785+dnesgurung@users.noreply.github.com>
2025-01-12 11:54:48 +0000

    Update index.js

--- Beginners-exercises/Destructuring-array-example/index.js
+++ Beginners-exercises/Destructuring-array-example/index.js
@@ -8,3 +8,12 @@ const [x, y, z] = arr;
 console.log(x, y, z); // 2 3 4
 console.log(arr); //  [2 ,3, 4]
 
+const restaurant = {
+  name: 'Golden Sip',
+  location: 'South harrow, London , UK',
+  categories: ['Momo', 'Chowmein', 'Thaali', 'Thukpa'],
+  starterMenu: ['Chatapte', 'Sekuwa', 'Choila', 'Sukuti'],
+};
+
+let[main, , secondary] = restaurant.categories;
+console.log(main, secondary); // Momo Thaali
