f2d02bb12c3b8b34e96430eb812753d0a13bb53b
dnesgurung <174604785+dnesgurung@users.noreply.github.com>
2025-02-08 16:21:09 +0000

    Update index.js

--- Beginners-exercises/Convert-Uppercase-To-Lowercase-And-Vice-Versa/index.js
+++ Beginners-exercises/Convert-Uppercase-To-Lowercase-And-Vice-Versa/index.js
@@ -1,21 +1,47 @@
 // 1. Using for of loop
 
-function swapCase(str){
+// function swapCase(str){
 
-    // Using split method
-    let letters = str.split('');
+//     // Using split method
+//     let letters = str.split('');
     
-    // Create an empty array to store
+//     // Create an empty array to store
+//     let newArray = [];
+//     // We use loop for of
+//     for ( let letter of letters){
+//         if (letter === letter.toUpperCase()){
+//             newArray.push(letter.toLowerCase())
+//         } else{
+//             newArray.push(letter.toUpperCase())
+//         }
+//     }
+//     return newArray.join('');
+// }
+
+
+// 2. Using for of and match method
+
+// function swapCase(str){
+//     let letters = str.split('');
+//     let newArray = [];
+//     for (letter of letters){
+//         if(letter.match('[A-Z]')){
+//             newArray.push(letter.toLowerCase())
+//         } else {
+//             newArray.push(letter.toUpperCase())
+//         }
+//     }
+//     return newArray.join('');
+// }
+
+
+// 3. using swap method
+
+function swapCase(str){
+    let letters = str.split('');
     let newArray = [];
-    // We use loop for of
-    for ( let letter of letters){
-        if (letter === letter.toUpperCase()){
-            newArray.push(letter.toLowerCase())
-        } else{
-            newArray.push(letter.toUpperCase())
-        }
-    }
-    return newArray.join('');
+    
+
 }
 
 let output = swapCase('AbCdE');
